00711ac535aa96dfacafb3026bcebee0d8a6c31a
Johannes L. Borresen <jlb@inqludeit.dk>
2023-05-08 15:03:25 +0200

    wip: ACTRulesReport component

---
+++ src/devtools/components/ActReportDisplay.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-data-table
+          :items="metadata"
+          :headers="[
+            {
+              title: 'Outcome',
+              key: 'outcome',
+            },
+            {
+              title: 'Count',
+              key: 'count'
+            }
+          ]"
+        ></v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import type { ACTRulesReport } from '@qualweb/act-rules';
+
+const { report } = defineProps<{ report: ACTRulesReport }>();
+
+const metadata = computed(() =>
+  Object.keys(report.metadata)
+    .map(k => ({
+      outcome: k,
+      count: report.metadata[k as keyof typeof report['metadata']],
+    }))
+);
+</script>
